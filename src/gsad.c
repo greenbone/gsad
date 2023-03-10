a344a9a06a43a3c6bc939864342751a7f872b4e0
Elmar Geese <elmar.geese@Elmars-MacBook-Pro.local>
2023-03-10 17:46:32 +0100

    Change Company name to Greenbone AG

--- src/gsad.c
+++ src/gsad.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2009-2021 Greenbone Networks GmbH
+/* Copyright (C) 2009-2021 Greenbone AG
  *
  * SPDX-License-Identifier: AGPL-3.0-or-later
  *
@@ -179,7 +179,7 @@
 #define DEFAULT_GSAD_PER_IP_CONNECTION_LIMIT 30
 
 #define COPYRIGHT                                                        \
-  "Copyright (C) 2010 - 2021 Greenbone Networks GmbH\n"                  \
+  "Copyright (C) 2010 - 2021 Greenbone AG\n"                  \
   "License: AGPL-3.0-or-later\n"                                         \
   "This is free software: you are free to change and redistribute it.\n" \
   "There is NO WARRANTY, to the extent permitted by law.\n\n"
