b819b7cd48c3e357f5daa8075012f2018570d7b0
Ahmed Abdelsalam <ahmed.abdelsalam@greenbone.net>
2024-02-02 11:23:24 +0100

    Fix code formatting

--- src/gsad_validator.c
+++ src/gsad_validator.c
@@ -469,11 +469,10 @@ init_validator ()
   gvm_validator_add (validator, "port_range_id", "^[a-z0-9\\-]+$");
   gvm_validator_add (
     validator, "resource_type",
-    "^(alert|asset|audit_report|cert_bund_adv|config|cpe|credential|cve|dfn_cert_adv|"
-    "filter|group|host|info|nvt|note|os|ovaldef|override|permission|port_list|"
-    "report|report_config|report_format|result|role|scanner|schedule|tag|"
-    "target|task|ticket|"
-    "tls_certificate|user|vuln|)$");
+    "^(alert|asset|audit_report|cert_bund_adv|config|cpe|credential|cve|"
+    "dfn_cert_adv|filter|group|host|info|nvt|note|os|ovaldef|override|"
+    "permission|port_list|report|report_config|report_format|result|role|scanner|"
+    "schedule|tag|target|task|ticket|tls_certificate|user|vuln|)$");
   gvm_validator_add (validator, "resource_id", "^[[:alnum:]\\-_.:\\/~]*$");
   gvm_validator_add (validator, "resources_action", "^(|add|set|remove)$");
   gvm_validator_add (
