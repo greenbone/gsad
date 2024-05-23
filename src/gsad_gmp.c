132c5837cb888213d8e2cc360d319cc652a7ad1a
Ahmed Abdelsalam <ahmed.abdelsalam@greenbone.net>
2024-05-23 15:44:32 +0200

    Fix: Comments should be escaped when creating or modifying targets.

--- src/gsad_gmp.c
+++ src/gsad_gmp.c
@@ -5336,7 +5336,8 @@ create_target_gmp (gvm_connection_t *connection, credentials_t *credentials,
   CHECK_VARIABLE_INVALID (allow_simultaneous_ips, "Create Target");
 
   if (comment != NULL)
-    comment_element = g_strdup_printf ("<comment>%s</comment>", comment);
+    comment_element =
+      g_markup_printf_escaped ("<comment>%s</comment>", comment);
   else
     comment_element = g_strdup ("");
 
@@ -6307,7 +6308,8 @@ save_target_gmp (gvm_connection_t *connection, credentials_t *credentials,
     entity_t entity;
 
     if (comment)
-      comment_element = g_strdup_printf ("<comment>%s</comment>", comment);
+      comment_element =
+        g_markup_printf_escaped ("<comment>%s</comment>", comment);
     else
       comment_element = g_strdup ("");
 
