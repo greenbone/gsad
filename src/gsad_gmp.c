face1328554ee69a45627859b2a486b1ea2c0868
Johannes Helmold <johannes.helmold@greenbone.net>
2025-06-11 14:42:10 +0200

    Fixed a little credential_id problem.
    Fixed a little problem regarding the credential_id that otherwise
    would occur when working with the new agent scanner.

--- src/gsad_gmp.c
+++ src/gsad_gmp.c
@@ -9901,7 +9901,8 @@ save_scanner_gmp (gvm_connection_t *connection, credentials_t *credentials,
                     "<comment>%s</comment>"
                     "<credential id=\"%s\"/>"
                     "</modify_scanner>",
-                    scanner_id, name, comment ?: "", credential_id);
+                    scanner_id, name, comment ?: "",
+                    credential_id ? credential_id : "");
       else
         ret = gmpf (connection, credentials, &response, &entity, response_data,
                     "<modify_scanner scanner_id=\"%s\">"
@@ -9913,7 +9914,7 @@ save_scanner_gmp (gvm_connection_t *connection, credentials_t *credentials,
                     "<credential id=\"%s\"/>"
                     "</modify_scanner>",
                     scanner_id, name, comment ?: "", host, port, type,
-                    credential_id);
+                    credential_id ? credential_id : "");
     }
 
   switch (ret)
