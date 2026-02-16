5cb35334eb89c80197ae5fc434d16c375616cde0
Björn Ricks <bjoern.ricks@greenbone.net>
2026-02-16 10:32:45 +0100

    Refactor setting the next handler in the chain
    Allow to call a set_leaf callback for handlers that branch like url and
    method handlers. When adding a next handler to those handlers they need
    to add the next handler on their leafs too. Therefore a callback is
    necessary for allowing that.

--- src/gsad_http_handler.c
+++ src/gsad_http_handler.c
@@ -24,57 +24,98 @@
  */
 
 /**
- * @brief Add a handler to the end of the handler chain.
- *
- * If the handler chain is empty, the new handler will be the first handler.
- * Otherwise, the new handler will be added to the end of the chain.
- *
- * @param[in] handlers The handler chain to add to.
- * @param[in] next The handler to add to the end of the chain.
- *
- * @return The head of the handler chain.
- */
-gsad_http_handler_t *
-gsad_http_handler_add (gsad_http_handler_t *handlers, gsad_http_handler_t *next)
-{
-  gsad_http_handler_t *handler = handlers;
-
-  if (handler == NULL)
-    {
-      return next;
-    }
-
-  while (handler->next != NULL)
-    {
-      handler = handler->next;
-    }
-
-  handler->next = next;
-
-  return handlers;
-}
-
-/**
- * @brief Set the next handler in the chain.
- *
- * If the handler is NULL, the next handler will be returned. Otherwise, the
- * next handler will be set and returned.
+ * @brief Set the next handler
  *
  * @param[in] handler The handler to set the next handler for.
  * @param[in] next The handler to set as the next handler.
+ * @param[in] free_next Whether to free the next handler when the handler is
+ * freed.
+ */
+void
+gsad_http_handler_set_next (gsad_http_handler_t *handler,
+                            gsad_http_handler_t *next, gboolean free_next)
+{
+  handler->free_next = free_next;
+  handler->next = next;
+}
+
+/**
+ * @brief Default set_leaf function for http handlers.
  *
- * @return The next handler in the chain.
+ * @param[in] handler The handler to set the next handler for.
+ * @param[in] next The handler to set as the next handler.
+ * @param[in] free_next Whether to free the next handler when the parent handler
+ * is freed.
+ */
+void
+gsad_http_handler_set_leaf (gsad_http_handler_t *handler,
+                            gsad_http_handler_t *next, gboolean free_next)
+{
+  if (handler->next)
+    {
+      // there are more handlers in the chaing, so we are not the leaf handler.
+      // Set the leaf handler in the next handler in the chain.
+      handler->next->set_leaf (handler->next, next, free_next);
+    }
+  else
+    {
+      // we are the leaf handler, so we set the next handler as the leaf
+      // handler.
+      gsad_http_handler_set_next (handler, next, free_next);
+    }
+}
+
+/**
+ * @brief Add a handler to the end of the handler chain.
+ *
+ * @param[in] handler The handler (chain) to add the next handler.
+ * @param[in] next The handler to add as the next handler.
+ * @param[in] free_next Whether to free the next handler when the parent
+ * handler is freed.
+ *
+ * @return The next handler
  */
 gsad_http_handler_t *
-gsad_http_handler_set_next (gsad_http_handler_t *handler,
-                            gsad_http_handler_t *next)
+gsad_http_handler_add_full (gsad_http_handler_t *handler,
+                            gsad_http_handler_t *next, gboolean free_next)
 {
   if (handler == NULL)
     {
       return next;
     }
-  handler->next = next;
+
+  handler->set_leaf (handler, next, free_next);
   return next;
+}
+
+/**
+ * @brief Add a handler to the end of the handler chain.
+ *
+ * @param[in] handler The handler (chain) to add the next handler.
+ * @param[in] next The handler to add as the next handler.
+ *
+ * @return The next handler
+ */
+gsad_http_handler_t *
+gsad_http_handler_add (gsad_http_handler_t *handler, gsad_http_handler_t *next)
+{
+  return gsad_http_handler_add_full (handler, next, TRUE);
+}
+
+/**
+ * @brief Add a handler to the end of the handler chain by creating a new
+ * handler with the given function.
+ *
+ * @param[in] handler The handler (chain) to add the next handler.
+ * @param[in] func The function to call when the next handler is called.
+ *
+ * @return The next handler
+ */
+gsad_http_handler_t *
+gsad_http_handler_add_from_func (gsad_http_handler_t *handler,
+                                 gsad_http_handler_func_t func)
+{
+  return gsad_http_handler_add (handler, gsad_http_handler_new (func));
 }
 
 /**
@@ -106,20 +147,26 @@ gsad_http_handler_call (gsad_http_handler_t *handler,
  * @brief Create a new HTTP handler with handler data
  *
  * @param[in] func The function to call when the handler is called.
- * @param[in] freefunc The function to call to free the handler data when the
+ * @param[in] free_func The function to call to free the handler data when the
  * handler is freed. Can be NULL if no special cleanup is needed.
+ * @param[in] set_leaf_func The function to call to set the leaf handler in the
+ * chain. Can be NULL to not support setting leaf handlers.
  * @param[in] handler_data The handler_data to pass to the handler function when
  * the handler is called.
  */
 gsad_http_handler_t *
-gsad_http_handler_new_with_data (gsad_http_handler_func_t func,
-                                 gsad_http_handler_free_func_t freefunc,
-                                 void *handler_data)
+gsad_http_handler_new_with_data (
+  gsad_http_handler_func_t func,
+  gsad_http_handler_set_leaf_func_t set_leaf_func,
+  gsad_http_handler_free_func_t free_func, void *handler_data)
 {
   gsad_http_handler_t *handler = g_malloc0 (sizeof (gsad_http_handler_t));
   handler->handle = func;
-  handler->free = freefunc;
   handler->data = handler_data;
+  handler->free = free_func;
+  handler->free_next = TRUE;
+  handler->set_leaf =
+    set_leaf_func ? set_leaf_func : gsad_http_handler_set_leaf;
   handler->next = NULL;
   return handler;
 }
@@ -138,7 +185,7 @@ gsad_http_handler_new_with_data (gsad_http_handler_func_t func,
 gsad_http_handler_t *
 gsad_http_handler_new (gsad_http_handler_func_t func)
 {
-  return gsad_http_handler_new_with_data (func, NULL, NULL);
+  return gsad_http_handler_new_with_data (func, NULL, NULL, NULL);
 }
 
 /**
@@ -156,14 +203,16 @@ gsad_http_handler_free (gsad_http_handler_t *handler)
   if (!handler)
     return;
 
-  if (handler->next)
+  if (handler->free_next && handler->next)
     {
       // free the chain
       gsad_http_handler_free (handler->next);
     }
+  handler->next = NULL;
   if (handler->free)
     {
       handler->free (handler->data);
+      handler->data = NULL;
     }
   g_free (handler);
 }
--- src/gsad_http_handler_internal.h
+++ src/gsad_http_handler_internal.h
@@ -8,12 +8,31 @@
 
 #include "gsad_http_handler.h"
 
+/**
+ * @brief Struct for handling HTTP requests.
+ */
 typedef struct gsad_http_handler
 {
-  gsad_http_handler_t *next;
-  gsad_http_handler_func_t handle;
-  gsad_http_handler_free_func_t free;
-  void *data;
+  gsad_http_handler_t *next; ///< next handler in the chain. to be called if the
+                             // current handler does not handle the request
+  gboolean free_next; ///< whether to free the next handler in the chain when
+                      ///< this handler is freed
+  gsad_http_handler_set_leaf_func_t
+    set_leaf; ///< function to set the leaf handler(s) in the chain
+  gsad_http_handler_func_t
+    handle; ///< function to call when the handler is called
+  gsad_http_handler_free_func_t free; ///< function to call to free the handler
+                                      ///< data when the handler is freed
+  void
+    *data; ///< data to pass to the handler function when the handler is called
 } gsad_http_handler_t;
+
+gsad_http_handler_t *
+gsad_http_handler_add_full (gsad_http_handler_t *, gsad_http_handler_t *,
+                            gboolean);
+
+void
+gsad_http_handler_set_leaf (gsad_http_handler_t *handler,
+                            gsad_http_handler_t *next, gboolean free_next);
 
 #endif /* _GSAD_HTTP_HANDLER_INTERNAL_H */
