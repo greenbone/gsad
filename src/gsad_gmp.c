1ba60917e4f2e6ad91722a6468b4f382f3877754
Matt Mundell <matt@mundell.me>
2026-03-17 12:20:49 +0200

    Change: use read_entity_c in save_target_gmp
    This one was leaking response.

--- src/gsad_gmp.c
+++ src/gsad_gmp.c
@@ -7613,7 +7613,7 @@ char *
 save_target_gmp (gvm_connection_t *connection, credentials_t *credentials,
                  params_t *params, cmd_response_data_t *response_data)
 {
-  gchar *html, *response;
+  gchar *html;
   const char *name, *comment, *target_id;
   const char *hosts = NULL, *exclude_hosts = NULL;
   const char *hosts_file = NULL, *exclude_hosts_file = NULL;
@@ -7842,7 +7842,7 @@ save_target_gmp (gvm_connection_t *connection, credentials_t *credentials,
     }
 
   entity = NULL;
-  if (read_entity_and_text_c (connection, &entity, &response))
+  if (read_entity_c (connection, &entity))
     {
       cmd_response_data_set_status_code (response_data,
                                          MHD_HTTP_INTERNAL_SERVER_ERROR);
