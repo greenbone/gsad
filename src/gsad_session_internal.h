7c25ce82b79029240b705a845a0e515517f9f33f
Björn Ricks <bjoern.ricks@greenbone.net>
2026-04-14 15:10:17 +0200

    test: Add tests for gsad session handling
    Ensure the session works as expected.

---
+++ src/gsad_session_internal.h
@@ -0,0 +1,17 @@
+/* Copyright (C) 2026 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+/**
+ * @file gsad_session_internal.h
+ * @brief GSAD user session internal functions
+ */
+
+#ifndef _GSAD_SESSION_INTERNAL_H
+#define _GSAD_SESSION_INTERNAL_H
+
+int
+gsad_session_get_user_count (void);
+
+#endif /* _GSAD_SESSION_INTERNAL_H */
---
+++ src/gsad_session_tests.c
@@ -0,0 +1,264 @@
+/* Copyright (C) 2026 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#include "gsad_session.h"
+#include "gsad_session_internal.h"
+#include "gsad_user.h"
+#include "gsad_user_internal.h"
+
+#include <cgreen/cgreen.h>
+
+int
+logout_gmp (const gchar *username, const gchar *password)
+{
+  return 0;
+}
+
+void
+gsad_user_session_renew_timeout (gsad_user_t *user)
+{
+  user->time = 0;
+}
+
+Describe (gsad_session);
+BeforeEach (gsad_session)
+{
+  gsad_session_init ();
+}
+AfterEach (gsad_session)
+{
+  gsad_session_cleanup ();
+}
+
+Ensure (gsad_session, should_add_and_get_user)
+{
+  gsad_user_t *user =
+    gsad_user_new_with_data ("username1", "password1", "timezone1",
+                             "capabilities1", "language1", "address1", "jwt1");
+
+  const gchar *token = gsad_user_get_token (user);
+  gsad_session_add_user (token, user);
+
+  assert_that (gsad_session_get_user_count (), is_equal_to (1));
+
+  gsad_user_t *retrieved_user = gsad_session_get_user_by_id (token);
+  assert_that (retrieved_user, is_not_null);
+  assert_that (gsad_user_get_username (retrieved_user),
+               is_equal_to_string ("username1"));
+  assert_that (gsad_user_get_password (retrieved_user),
+               is_equal_to_string ("password1"));
+  assert_that (gsad_user_get_timezone (retrieved_user),
+               is_equal_to_string ("timezone1"));
+  assert_that (gsad_user_get_capabilities (retrieved_user),
+               is_equal_to_string ("capabilities1"));
+  assert_that (gsad_user_get_language (retrieved_user),
+               is_equal_to_string ("language1"));
+  assert_that (gsad_user_get_client_address (retrieved_user),
+               is_equal_to_string ("address1"));
+  assert_that (gsad_user_get_jwt (retrieved_user), is_equal_to_string ("jwt1"));
+
+  gsad_user_free (user);
+  gsad_user_free (retrieved_user);
+}
+
+Ensure (gsad_session, should_allow_to_add_user_twice)
+{
+  gsad_user_t *user =
+    gsad_user_new_with_data ("username1", "password1", "timezone1",
+                             "capabilities1", "language1", "address1", "jwt1");
+  gsad_user_t *user_copy = gsad_user_copy (user);
+
+  gsad_session_add_user (gsad_user_get_token (user), user);
+  assert_that (gsad_session_get_user_count (), is_equal_to (1));
+  gsad_session_add_user (gsad_user_get_token (user_copy), user_copy);
+  assert_that (gsad_session_get_user_count (), is_equal_to (1));
+
+  gsad_user_t *retrieved_user =
+    gsad_session_get_user_by_id (gsad_user_get_token (user));
+  assert_that (retrieved_user, is_not_null);
+
+  gsad_user_free (retrieved_user);
+
+  gsad_user_t *user2 =
+    gsad_user_new_with_data ("username1", "password1", "timezone1",
+                             "capabilities1", "language1", "address1", "jwt1");
+  gsad_session_add_user (gsad_user_get_token (user2), user2);
+
+  assert_that (gsad_session_get_user_count (), is_equal_to (2));
+  retrieved_user = gsad_session_get_user_by_id (gsad_user_get_token (user2));
+  assert_that (retrieved_user, is_not_null);
+
+  gsad_user_free (retrieved_user);
+  gsad_user_free (user_copy);
+  gsad_user_free (user2);
+  gsad_user_free (user);
+}
+
+Ensure (gsad_session, should_allow_to_get_users_by_username)
+{
+  gsad_user_t *user1 =
+    gsad_user_new_with_data ("username1", "password1", "timezone1",
+                             "capabilities1", "language1", "address1", "jwt1");
+  gsad_user_t *user2 =
+    gsad_user_new_with_data ("username1", "password2", "timezone2",
+                             "capabilities2", "language2", "address2", "jwt2");
+  gsad_user_t *user3 =
+    gsad_user_new_with_data ("username2", "password3", "timezone3",
+                             "capabilities3", "language3", "address3", "jwt3");
+
+  gsad_session_add_user (gsad_user_get_token (user1), user1);
+  gsad_session_add_user (gsad_user_get_token (user2), user2);
+  gsad_session_add_user (gsad_user_get_token (user3), user3);
+
+  GList *users = gsad_session_get_users_by_username ("username1");
+  assert_that (g_list_length (users), is_equal_to (2));
+
+  GList *first = users;
+  assert_that (gsad_user_get_password ((gsad_user_t *) first->data),
+               is_equal_to_string ("password2"));
+  first = first->next;
+  assert_that (gsad_user_get_password ((gsad_user_t *) first->data),
+               is_equal_to_string ("password1"));
+
+  g_list_free_full (users, (GDestroyNotify) gsad_user_free);
+
+  gsad_user_free (user1);
+  gsad_user_free (user2);
+  gsad_user_free (user3);
+}
+
+Ensure (gsad_session,
+        should_allow_to_call_gsad_session_get_user_by_id_with_null)
+{
+  gsad_user_t *retrieved_user = gsad_session_get_user_by_id (NULL);
+  assert_that (retrieved_user, is_null);
+}
+
+Ensure (gsad_session,
+        should_allow_to_call_gsad_session_get_users_by_username_with_null)
+{
+  GList *users = gsad_session_get_users_by_username (NULL);
+  assert_that (users, is_null);
+}
+
+Ensure (
+  gsad_session,
+  should_allow_to_call_gsad_session_get_users_by_username_with_no_matching_user)
+{
+  GList *users = gsad_session_get_users_by_username ("nonexistent_username");
+  assert_that (users, is_null);
+}
+
+Ensure (gsad_session, should_allow_to_remove_user)
+{
+  gsad_user_t *user =
+    gsad_user_new_with_data ("username1", "password1", "timezone1",
+                             "capabilities1", "language1", "address1", "jwt1");
+
+  const gchar *token = gsad_user_get_token (user);
+  gsad_session_add_user (token, user);
+
+  assert_that (gsad_session_get_user_count (), is_equal_to (1));
+
+  gsad_session_remove_user (token);
+
+  assert_that (gsad_session_get_user_count (), is_equal_to (0));
+
+  gsad_user_free (user);
+}
+
+Ensure (gsad_session, should_allow_to_remove_user_with_null)
+{
+  gsad_session_remove_user (NULL);
+  assert_that (gsad_session_get_user_count (), is_equal_to (0));
+}
+
+Ensure (gsad_session, should_remove_other_sessions)
+{
+  gsad_user_t *user1 =
+    gsad_user_new_with_data ("username1", "password1", "timezone1",
+                             "capabilities1", "language1", "address1", "jwt1");
+  gsad_user_t *user2 =
+    gsad_user_new_with_data ("username1", "password2", "timezone2",
+                             "capabilities2", "language2", "address2", "jwt2");
+  gsad_user_t *user3 =
+    gsad_user_new_with_data ("username1", "password3", "timezone3",
+                             "capabilities3", "language3", "address3", "jwt3");
+
+  gsad_session_add_user (gsad_user_get_token (user1), user1);
+  gsad_session_add_user (gsad_user_get_token (user2), user2);
+  gsad_session_add_user (gsad_user_get_token (user3), user3);
+
+  assert_that (gsad_session_get_user_count (), is_equal_to (3));
+
+  gsad_session_remove_other_sessions (gsad_user_get_token (user1),
+                                      gsad_user_get_username (user1));
+
+  assert_that (gsad_session_get_user_count (), is_equal_to (1));
+
+  GList *users = gsad_session_get_users_by_username ("username1");
+  assert_that (g_list_length (users), is_equal_to (1));
+
+  g_list_free_full (users, (GDestroyNotify) gsad_user_free);
+
+  gsad_user_free (user1);
+  gsad_user_free (user2);
+  gsad_user_free (user3);
+}
+
+Ensure (gsad_session, should_renew_user)
+{
+  gsad_user_t *user =
+    gsad_user_new_with_data ("username1", "password1", "timezone1",
+                             "capabilities1", "language1", "address1", "jwt1");
+
+  const gchar *token = gsad_user_get_token (user);
+  time_t timeout = gsad_user_get_time (user);
+  gsad_session_add_user (token, user);
+
+  gsad_user_t *retrieved_user = gsad_session_get_user_by_id (token);
+  assert_that (gsad_session_get_user_count (), is_equal_to (1));
+  assert_that (gsad_user_get_time (retrieved_user), is_equal_to (timeout));
+
+  gsad_session_renew_user (token);
+
+  assert_that (gsad_session_get_user_count (), is_equal_to (1));
+  assert_that (gsad_user_get_time (gsad_session_get_user_by_id (token)),
+               is_not_equal_to (timeout));
+  assert_that (gsad_user_get_time (gsad_session_get_user_by_id (token)),
+               is_equal_to (0));
+
+  gsad_user_free (user);
+  gsad_user_free (retrieved_user);
+}
+
+int
+main (int argc, char **argv)
+{
+  TestSuite *suite = create_test_suite ();
+
+  add_test_with_context (suite, gsad_session, should_add_and_get_user);
+  add_test_with_context (suite, gsad_session, should_allow_to_add_user_twice);
+  add_test_with_context (suite, gsad_session,
+                         should_allow_to_get_users_by_username);
+  add_test_with_context (
+    suite, gsad_session,
+    should_allow_to_call_gsad_session_get_user_by_id_with_null);
+  add_test_with_context (
+    suite, gsad_session,
+    should_allow_to_call_gsad_session_get_users_by_username_with_null);
+  add_test_with_context (
+    suite, gsad_session,
+    should_allow_to_call_gsad_session_get_users_by_username_with_no_matching_user);
+  add_test_with_context (suite, gsad_session, should_allow_to_remove_user);
+  add_test_with_context (suite, gsad_session,
+                         should_allow_to_remove_user_with_null);
+  add_test_with_context (suite, gsad_session, should_remove_other_sessions);
+  add_test_with_context (suite, gsad_session, should_renew_user);
+
+  int ret = run_test_suite (suite, create_text_reporter ());
+  destroy_test_suite (suite);
+  return ret;
+}
