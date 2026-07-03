81a5391765e0363c8d64929e7545ba9c595dfdaa
ozgen <ozgn45@gmail.com>
2026-07-03 16:02:30 +0200

    fix: pass report filters to get_report_closed_cves

--- src/gsad_gmp.c
+++ src/gsad_gmp.c
@@ -10083,19 +10083,33 @@ get_report_closed_cves_gmp (gvm_connection_t *connection,
   GString *xml;
   entity_t entity;
   const char *report_id;
+  const char *filter;
+  const char *filter_id;
   gboolean details;
+  gboolean ignore_pagination;
   int ret;
 
   details = params_value_bool (params, "details");
+  ignore_pagination = params_value_bool (params, "ignore_pagination");
+
   report_id = params_value (params, "report_id");
+  filter = params_value (params, "filter");
+  filter_id = params_value (params, "filter_id");
 
   CHECK_VARIABLE_INVALID (report_id, "Get Report Closed CVEs");
+
+  if (filter == NULL || filter_id)
+    filter = "";
 
   ret = gvm_connection_sendf_xml (connection,
                                   "<get_report_closed_cves"
                                   " report_id=\"%s\""
-                                  " details=\"%d\"/>",
-                                  report_id, details);
+                                  " details=\"%d\""
+                                  " ignore_pagination=\"%d\""
+                                  " filter=\"%s\""
+                                  " filt_id=\"%s\"/>",
+                                  report_id, details, ignore_pagination, filter,
+                                  filter_id ? filter_id : FILT_ID_NONE);
 
   if (ret == -1)
     {
